664717dd4e21c304123977dbb034b8d35154e93c
Rajat Kumar <rjtkr2@gmail.com>
2026-01-29 10:51:03 +0530

    add Dice button

--- lib/main.dart
+++ lib/main.dart
@@ -17,29 +17,54 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class GameScreen extends StatelessWidget {
+class GameScreen extends StatefulWidget {
   const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  int playerPosition = 1;
+
+  void rollDice() {
+    setState(() {
+      int dice = (1 + (6 * (DateTime.now().millisecondsSinceEpoch % 6) / 6))
+          .floor();
+      playerPosition += dice;
+      if (playerPosition > 100) playerPosition = 100;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Snake and Ladder")),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(8),
-        itemCount: 100,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 10,
-        ),
-        itemBuilder: (context, index) {
-          int cellNumber = 100 - index;
-          return Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black),
-              color: index % 2 == 0 ? Colors.green[100] : Colors.green[200],
+      body: Column(
+        children: [
+          Expanded(
+            child: GridView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: 100,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 10,
+              ),
+              itemBuilder: (context, index) {
+                int cellNumber = 100 - index;
+                bool isPlayerHere = cellNumber == playerPosition;
+
+                return Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    color: isPlayerHere ? Colors.red : Colors.green[200],
+                  ),
+                  child: Center(child: Text(cellNumber.toString())),
+                );
+              },
             ),
-            child: Center(child: Text(cellNumber.toString())),
-          );
-        },
+          ),
+          ElevatedButton(onPressed: rollDice, child: const Text("Roll Dice")),
+        ],
       ),
     );
   }
