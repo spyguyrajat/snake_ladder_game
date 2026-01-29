8ccf7d3bd36ea26b42d87b342650322269634a4e
Rajat Kumar <rjtkr2@gmail.com>
2026-01-29 10:16:16 +0530

    add initial GameScreen UI

---
+++ android/app/src/main/kotlin/com/example/snake_ladder_game/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.snake_ladder_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GameScreen(),
+    );
+  }
+}
+
+class GameScreen extends StatelessWidget {
+  const GameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Snake and Ladder")),
+      body: const Center(child: Text("Game coming soon")),
+    );
+  }
+}
