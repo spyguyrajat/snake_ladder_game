d75d39bf32315b871da9eb71b8acb4515a1d04a1
Rajat Kumar <rjtkr2@gmail.com>
2026-01-29 10:34:56 +0530

    add 10x10 game board UI

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,23 @@ class GameScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Snake and Ladder")),
-      body: const Center(child: Text("Game coming soon")),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: 100,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 10,
+        ),
+        itemBuilder: (context, index) {
+          int cellNumber = 100 - index;
+          return Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              color: index % 2 == 0 ? Colors.green[100] : Colors.green[200],
+            ),
+            child: Center(child: Text(cellNumber.toString())),
+          );
+        },
+      ),
     );
   }
 }
